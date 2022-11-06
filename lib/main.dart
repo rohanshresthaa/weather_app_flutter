b479fe93febf8e4efd051c23b99447070f5492c2
rohanshresthaa <r.shrestharohan@gmail.com>
2022-11-06 21:16:31 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/Loading.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: LoadingScreen(),
+    ),
+  );
+}
